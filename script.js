d19620e5b100e7ea13e588e5cf53c159391281ea
Arthur Lacerda <arthur.srl@gmail.com>
2022-09-04 00:50:57 -0300

    Algoritmos Logica Programacao JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Olá mundo!')
+//alert('Now I programming JS!') Comentando em uma linha.
+
+/* 
+    Encontre a solução do problema:
+    Pergunte nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]"
+
+    Como resolver problemas na liguagem de programação?
+    Como melhorar minha lógica de programação?
+
+    1. Identifique o que já sabe e faça!
+    2. Quebre o grande problema em problemas menores!
+*/
+
+//prompt - captar argumentos em tela
+
+let nameUser = ''
+nameUser = prompt('Digite o nome do usuário?')
+alert('Olá, '+ nameUser)
